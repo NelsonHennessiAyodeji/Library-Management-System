dfe3a0042dd92fbaa1f42c4992425cb895c0ee3b
NelsonHennessiAyodeji <nelsonhennessiayodeji@gmail.com>
2024-12-13 17:56:45 +0100

    Update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,31 +1,33 @@
 const { verifyToken } = require("../utilities");
 const { UnauthenticatedError, UnauthorizedError } = require("../error");
-const path = require('path');
+const path = require("path");
 
 const authenticateUser = (req, res, next) => {
-    const {token} = req.signedCookies;
+  const { token } = req.signedCookies;
 
-    if(!token || token == null){
-        res.sendFile(path.join(__dirname, '../public/login-register.html'));
-        throw new UnauthenticatedError("Invalid Token");
-    }
+  if (!token || token == null) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+    throw new UnauthenticatedError("Invalid Token");
+  }
 
-    const {userName, userEmail, userRole, userId} = verifyToken(token);
-    req.user = {userName, userEmail, userRole, userId};
-    next();
+  const { userName, userEmail, userRole, userId } = verifyToken(token);
+  req.user = { userName, userEmail, userRole, userId };
+  next();
 };
 
 const authorizePermissions = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.userRole)){
-            res.sendFile(path.join(__dirname, 'public/login-register.html'));
-            throw new UnauthorizedError("You are not high level enough to access this route");
-        }
-        next();
+  return (req, res, next) => {
+    if (!roles.includes(req.user.userRole)) {
+      res.sendFile(path.join(__dirname, "public/login.html"));
+      throw new UnauthorizedError(
+        "You are not high level enough to access this route"
+      );
     }
+    next();
+  };
 };
 
 module.exports = {
-    authenticateUser,
-    authorizePermissions
+  authenticateUser,
+  authorizePermissions,
 };
--- server.js
+++ server.js
@@ -42,7 +42,10 @@ const { error } = require("console");
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin:
+      process.env.NODE_ENV === "Production"
+        ? "https://library-management-system-d2rd.onrender.com"
+        : "http://127.0.0.1:5500",
     methods: ["GET", "POST"],
   })
 );
